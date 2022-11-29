98a0f63e0c050ad8594c041d251c48a14bbc4c6e
jenell <Jenell.E.Rotter@phila.gov>
2022-11-29 15:17:06 -0500

    Passing tests

---
+++ MathFunctionUnitTests/UnitTest2.cs
@@ -0,0 +1,30 @@
+﻿using DemoTestApp;
+using DemoTestApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Moq;
+
+namespace MathFunctionUnitTests;
+
+public class UnitTest2
+{
+    private readonly IMathFunctionsService _sut;
+
+
+    public UnitTest2()
+    {
+        _sut = new MathFunctionsService();
+    }
+
+    [Fact]
+    public void AddNumbersServiceReturnsCorrectValue()
+    {
+        //Arrange
+        //Act
+        var result = _sut.AddNumbers(35, 22);
+        var exp = 35 + 22;
+
+        //Assert
+        Assert.True(exp == result);
+    }
+}
